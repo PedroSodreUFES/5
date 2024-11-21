1780f51537df487bc48dcab046371f158476200f
PedroSodreUFES <pedro.malini@edu.ufes.br>
2024-11-21 14:49:32 -0300

    21/11 done

--- POO/11-21-24/FuncionarioTempoIntegral.java
+++ POO/11-21-24/FuncionarioTempoIntegral.java
@@ -1,15 +1,12 @@
 class FuncionarioTempoIntegral extends Funcionario
 {
-    private int horasSemanais;
-
     public FuncionarioTempoIntegral(String nome, float salario)
     {
         super(nome, salario);
-        this.horasSemanais = 44;
     }
 
     public int getNumeroHorasSemanais()
     {
-        return this.horasSemanais;
+        return 44;
     }
 }
